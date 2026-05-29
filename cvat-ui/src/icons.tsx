81a8637d1377a44f6818888f404a3e65a27d40ba
chunwei <U12627017@o365.mcut.edu.tw>
2026-05-29 12:17:42 +0800

    更改 CVAT 標誌圖示的檔案路徑

--- cvat-ui/src/icons.tsx
+++ cvat-ui/src/icons.tsx
@@ -5,7 +5,7 @@
 
 import React from 'react';
 
-import SVGCVATIcon from './assets/iLabel-icon.svg';
+import SVGCVATIcon from './assets/cvat-icon.svg';
 import SVGCursorIcon from './assets/cursor-icon.svg';
 import SVGMoveIcon from './assets/move-icon.svg';
 import SVGRotateIcon from './assets/rotate-icon.svg';
